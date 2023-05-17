64220aa0419384989eabe5aeb0713f17c7e19f6f
bee <bb.bee.bud@gmail.com>
2023-05-17 13:36:43 +1000

    assignment3

--- constants.php
+++ constants.php
@@ -33,31 +33,31 @@
 */
 
 // [ACTION REQUIRED] your full name
-define('STUDENT_NAME', 'Bee Boossadee');
+define('STUDENT_NAME', 'Bee Boossadee, Danyang Liu, Howard Hao');
 // [ACTION REQUIRED] your Student ID
-define('STUDENT_ID', '103517930');
+define('STUDENT_ID', '103517930, 103902523, 10411825');
 // [ACTION REQUIRED] your tutorial session
-define('TUTORIAL_SESSION', 'Thursday 06:30PM');
+define('TUTORIAL_SESSION', 'Wednesday and Thursday 06:30PM');
 
 // [ACTION REQUIRED] name of the object storage bucket that stores images
-define('BUCKET_NAME', 'Assignment2_Bucket');
+define('BUCKET_NAME', 'Assignment3_Bucket');
 // [ACTION REQUIRED] region of the above bucket
 define('REGION', 'us-ashburn-1');
 // no need to update this const
 define('S3_BASE_URL','
-https://objectstorage.us-ashburn-1.oraclecloud.com/n/axkl7eiitqos/b/Assignment2_Bucket/o/');
+https://objectstorage.us-ashburn-1.oraclecloud.com/n/axkl7eiitqos/b/Assignment3_Bucket/o');
 
 // [ACTION REQUIRED] name of the database that stores photo meta-data (note that this is not the DB identifier of the database instance)
-define('DB_NAME', 'photos');
+define('DB_NAME', 'assignment3DB');
 // [ACTION REQUIRED] endpoint/private IP of the MySQL database
-define('DB_ENDPOINT', '172.17.3.68');
+define('DB_ENDPOINT', '172.17.3.102');
 // [ACTION REQUIRED] administrator username of your database  
 define('DB_USERNAME', 'admin');
 // [ACTION REQUIRED] administrator password of your database 
-define('DB_PWD', 'Oraclecloud@2023');
+define('DB_PWD', 'DBpassword@2023');
 
 // [ACTION REQUIRED] name of the DB table that stores photo's meta-data
-define('DB_PHOTO_TABLE_NAME', 'photos');
+define('DB_PHOTO_TABLE_NAME', 'photosDB');
 // The table above has 5 columns:
 // [ACTION REQUIRED] name of the column in the above table that stores photo's titles
 define('DB_PHOTO_TITLE_COL_NAME', 'photo_title');
